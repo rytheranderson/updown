2c7cf755be14e30759f5dfd1a9ca1ae2cce35e17
rytheranderson <rytheranderson@mymail.mines.edu>
2021-08-30 12:08:27 -0600

    general syntax updates

--- Ising_Metropolis_MC.py
+++ Ising_Metropolis_MC.py
@@ -22,153 +22,190 @@ cmap1 = colors.ListedColormap(('#8e82fe','#580f41')) #periwinkle and plum
 @jit
 def initialize_lattice_random(nrow, ncol):
 
-	return np.where(np.random.random((nrow,ncol))>0.5,1,-1)
+    """
+        initialize a lattice with randomly oriented spins
+    """
+
+    return np.where(np.random.random((nrow,ncol))>0.5,1,-1)
 
 @jit
 def initialize_lattice_uniform(nrow, ncol, spin=1):
 
-	return np.ones((nrow,ncol), dtype=np.int64) * spin
+    """
+        initialize a lattice with spins all in the same direction
+    """
+
+    return np.ones((nrow,ncol), dtype=np.int64) * spin
 
 @jit
 def system_energy(lattice, J, H):
 
-	nrow,ncol = lattice.shape
-	E = 0.0
+    """
+        J is the spin interaction parameter, J > 0 = ferromagnetic, J < 0 = antiferromagnetic
+        H is an external magnetic field (constant)
+    """
 
-	for i in range(nrow):
-		for j in range(ncol):
+    nrow,ncol = lattice.shape
+    E = 0.0
 
-			S  = lattice[i,j]
-			NS = lattice[(i+1)%nrow, j] + lattice[i,(j+1)%ncol] + lattice[(i-1)%nrow, j] + lattice[i,(j-1)%ncol]
-			E += -1 * ( (J * S * NS) + (H * S) )
+    for i in range(nrow):
+        for j in range(ncol):
 
-	return E/4
+            S  = lattice[i,j]
+            NS = lattice[(i+1)%nrow, j] + lattice[i,(j+1)%ncol] + lattice[(i-1)%nrow, j] + lattice[i,(j-1)%ncol]
+            E += -1 * ((J * S * NS) + (H * S))
+
+    return E/4
 
 @jit
 def system_magnetization(lattice):
 
-	return np.sum(lattice)
+    """
+        calculate the system magnetization, just a rename of np.sum for physical interpretation
+    """
+
+    return np.sum(lattice)
 
 @jit
 def MC_cycle(lattice, J, H, T):
 
-	T = float(T)
-	naccept = 0 
-	nrow,ncol = lattice.shape 
-	E = system_energy(lattice, J, H) 
-	M = system_magnetization(lattice)
+    """
+        A single MC cycle (considering all lattice points)
+        T is the temperature
+    """
 
-	for i in range(nrow): 
-		for j in range(ncol):
+    T = float(T)
+    naccept = 0 
+    nrow,ncol = lattice.shape 
+    E = system_energy(lattice, J, H) 
+    M = system_magnetization(lattice)
 
-			S  = lattice[i,j]
-			NS = lattice[(i+1)%nrow, j] + lattice[i,(j+1)%ncol] + lattice[(i-1)%nrow, j] + lattice[i,(j-1)%ncol]
-			dE = 2*J*S*NS + 2*H*S
-			accept = np.random.random()
+    for i in range(nrow): 
+        for j in range(ncol):
 
-			if dE < 0.0 or accept < np.exp( (-1.0 * dE) / T ):
-				naccept += 1
-				S *= -1
-				E += dE
-				M += 2*S
+            S = lattice[i,j]
+            NS = lattice[(i+1)%nrow, j] + lattice[i,(j+1)%ncol] + lattice[(i-1)%nrow, j] + lattice[i,(j-1)%ncol]
+            dE = 2*J*S*NS + 2*H*S
+            accept = np.random.random()
 
-			lattice[i,j] = S
+            if dE < 0.0 or accept < np.exp((-1.0 * dE)/T):
+                naccept += 1
+                S *= -1
+                E += dE
+                M += 2*S
 
-	return lattice, E, M, naccept
+            lattice[i,j] = S
+
+    return lattice, E, M, naccept
 
 @jit
-def run(lattice, J, H, T, N_cycles, standard_output=False):
+def run(lattice, N_cycles, J=1, H=0, T=1.0, standard_output=False):
 
-	nrow,ncol = lattice.shape
+    """
+        The summary function, which runs an MC simulation of N_cycles
+    """
 
-	lattice_evolve = [np.zeros((nrow,ncol)) for i in range(N_cycles)]
-	energy_vs_step = []
-	magnet_vs_step = []
+    nrow,ncol = lattice.shape
 
-	for cyc in range(N_cycles):
+    lattice_evolve = [np.zeros((nrow,ncol)) for i in range(N_cycles)]
+    energy_vs_step = []
+    magnet_vs_step = []
 
-		if standard_output:
-			print('cycle', cyc + 1, 'out of', N_cycles)
+    for cyc in range(N_cycles):
 
-		lattice,E,M,naccept = MC_cycle(lattice, J, H, T)
-		lattice_evolve[cyc] += lattice
-		energy_vs_step.append(E)
-		magnet_vs_step.append(M)
+        if standard_output:
+            print('cycle', cyc + 1, 'out of', N_cycles)
 
-	return lattice, energy_vs_step, magnet_vs_step, lattice_evolve
+        lattice,E,M,naccept = MC_cycle(lattice, J, H, T)
+        lattice_evolve[cyc] += lattice
+        energy_vs_step.append(E)
+        magnet_vs_step.append(M)
+
+    return lattice, energy_vs_step, magnet_vs_step, lattice_evolve
 
 @jit
 def cooling(lattice, T_range, N_cycles, J=1, H=0):
 
-	summary = []
-	for T in T_range:
+    """
+        a series of runs at decreasing temperatures
+    """
 
-		FL, EvS, MvS, LvS , N_converge = run(lattice, J, H, T, N_cycles)
-		summary.append([J, EvS, MvS, LvS])
+    summary = []
+    for T in T_range:
 
-	return summary
+        FL, EvS, MvS, LvS , N_converge = run(lattice, J, H, T, N_cycles)
+        summary.append([J, EvS, MvS, LvS])
+
+    return summary
 
 def animate_run(LvS, size=(5,5), fps=15, bitrate=1800, filename='run', ticks='off', dpi=100, cmap=cmap1):
 
-	writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate=bitrate)
-	FIG = plt.figure(figsize=size)
-	ims = []
-	plt.axis(ticks)
-	
-	for L in LvS:
-		
-		L = np.where(L==1.0, 0, 255)
-		im = plt.imshow(L, origin='lower', cmap=cmap)
-		ims.append([im])
+    """
+        function used to produce run animations
+    """
 
-	ani = animation.ArtistAnimation(FIG, ims, interval=50, blit=True, repeat_delay=1000)
-	ani.save(filename + '.gif', dpi=dpi, writer=writer)
+    writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate=bitrate)
+    FIG = plt.figure(figsize=size)
+    ims = []
+    plt.axis(ticks)
+    
+    for L in LvS:
+        
+        L = np.where(L==1.0, 0, 255)
+        im = plt.imshow(L, origin='lower', cmap=cmap)
+        ims.append([im])
+
+    ani = animation.ArtistAnimation(FIG, ims, interval=50, blit=True, repeat_delay=1000)
+    ani.save(filename + '.gif', dpi=dpi, writer=writer)
 
 def animate_cooling(summary, size=(5,5), fps=15, bitrate=1800, filename='vary', ticks='off', dpi=100, cmap=cmap1):
 
-	writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate=bitrate)
-	FIG = plt.figure(figsize=size)
-	ims = []
-	plt.axis(ticks)
-	
-	for line in summary:
+    """
+        animate a cooling simulation
+    """
 
-		var, EvS, MvS, LvS = line
+    writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate=bitrate)
+    FIG = plt.figure(figsize=size)
+    ims = []
+    plt.axis(ticks)
+    
+    for line in summary:
 
-		for L in LvS:
+        var, EvS, MvS, LvS = line
 
-			L = np.where(L==1.0, 0, 255)
-			im = plt.imshow(L, origin='lower', cmap=cmap)
-			ims.append([im])
+        for L in LvS:
 
-	ani = animation.ArtistAnimation(FIG, ims, interval=50, blit=True, repeat_delay=1000)
-	ani.save(filename + '.gif', dpi=dpi, writer=writer)
+            L = np.where(L==1.0, 0, 255)
+            im = plt.imshow(L, origin='lower', cmap=cmap)
+            ims.append([im])
+
+    ani = animation.ArtistAnimation(FIG, ims, interval=50, blit=True, repeat_delay=1000)
+    ani.save(filename + '.gif', dpi=dpi, writer=writer)
 
 #------------------------------------------------------------------------------# 
 # Usage example
 #------------------------------------------------------------------------------#
 
-start_time = time.time()
-L = initialize_lattice_random(100, 100)
-#L = initialize_lattice_uniform(200, 200)
-FL, EvS, MvS, LvS = run(L, 0.8, 0, 1.0, 1000, standard_output=True)
-animate_run(LvS)
+if __name__ == '__main__':
 
-### Plot of E vs step
-
-fig = plt.figure(figsize=(4,3))
-plt.xlabel('MC Cycles')
-plt.ylabel('Energy')
-plt.plot(range(len(EvS)), EvS)
-plt.savefig('EvS.tiff', dpi=300)
-plt.close(fig)
-
-fig = plt.figure(figsize=(4,3))
-plt.xlabel('MC Cycles')
-plt.ylabel('Magnetization')
-plt.plot(range(len(MvS)), MvS)
-plt.savefig('MvS.tiff', dpi=300)
-plt.close(fig)
-
-print('Normal termination after')
-print('--- %s seconds ---' % (time.time() - start_time))
+    start_time = time.time()
+    L = initialize_lattice_random(200, 200)
+    FL, EvS, MvS, LvS = run(L, 300, J=0.75, H=0, T=1.0, standard_output=True)
+    animate_run(LvS)
+        
+    fig = plt.figure(figsize=(4,3))
+    plt.xlabel('MC Cycles')
+    plt.ylabel('Energy')
+    plt.plot(range(len(EvS)), EvS)
+    plt.savefig('EvS.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+    
+    fig = plt.figure(figsize=(4,3))
+    plt.xlabel('MC Cycles')
+    plt.ylabel('Magnetization')
+    plt.plot(range(len(MvS)), MvS)
+    plt.savefig('MvS.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+    
+    print('Normal termination after')
+    print('--- %s seconds ---' % (time.time() - start_time))
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Ising_model
 
-Python 2/3 code for running 1-3D Ising models and visualizing them.
+Python 3 code for running 1 to 3D Ising models and visualizing the results. 
+This code was designed to be simple and fast, allowing for the simulations of relatively large lattices.
+Keep in mind that the runs are fast, but the animation is not.
 
-Install ffmpeg for writing the animations using Matplotlib:
+<p align="center">
+<img src="./run.gif" width="800" height="600"/>
+</p>
+
+An example run animated. This run corresponds to a ferromagnetic 200x200 lattice that was randomly initialized.
+Here is the code used to produce the image:
+
 ```
-conda install -c conda-forge ffmpeg
+L = initialize_lattice_uniform(200, 200)
+FL, EvS, MvS, LvS = run(L, 1000, J=0.8, H=0, T=1.0, standard_output=True)
+animate_run(LvS)
 ```
