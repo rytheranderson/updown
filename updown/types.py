ea1b0ecf8cba488c77c0f437fe788ed4ca9d746a
Ryther Anderson <rytheranderson@gmail.com>
2023-05-27 13:53:05 -0600

    Update types

--- updown/types.py
+++ updown/types.py
@@ -1,4 +1,8 @@
 """Types used throughout updown."""
+from typing import NewType
+
 from nptyping import Int64, NDArray, Shape
 
+Width = NewType("Width", int)
+Height = NewType("Height", int)
 Lattice = NDArray[Shape["Height, Width"], Int64]
